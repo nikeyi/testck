49101f7b7d99e8c4da5e72ff598c69e185f4928f
wp <1269665226@qq.com>
2022-06-09 20:55:18 +0800

    第一次提交的东西

---
+++ src/main/java/com/wp/test.java
@@ -0,0 +1,13 @@
+package com.wp;
+
+/**
+ * @Auther: wp
+ * @Date: 2022/6/9 - 06 - 09 - 20:53
+ * @Description: com.wp
+ * @version: 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("这是我要add的东西");
+    }
+}
